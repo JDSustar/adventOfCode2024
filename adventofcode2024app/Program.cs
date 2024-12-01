de77d51d9d2738c89ff1105112411beeba5a3af2
Jason Sustar <jason.sus@gmail.com>
2024-11-30 20:13:17 -0500

    initial commit

---
+++ adventofcode2024app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventofcode2024app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
